fd6116e5ae9a0db632db4e3e14fcb0222d58223d
mouadh.-dev <mouadh.laa@gmail.com>
2021-10-27 17:48:52 +0100

    animation

--- app/src/main/java/com/example/writingthevoice/MainActivity.kt
+++ app/src/main/java/com/example/writingthevoice/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.writingthevoice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    var animatedName:TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initView()
+    }
+
+    private fun initView() {
+        animatedName = findViewById(R.id.nameAnimated)
+        animatedName!!.startAnimation(AnimationUtils.loadAnimation(this,R.anim.rotate))
+
     }
 }
